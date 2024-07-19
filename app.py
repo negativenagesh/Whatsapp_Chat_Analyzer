3870a50d1f737d12e9f23cda8281e44292ca2cbf
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-19 17:14:02 +0530

    modified app.py by adding sidebar to upload a file and added a variable which converts byte data to string using utf-8, after this the uploaded data is passed to the preprocess function in preproessor.py and after this function runs, it will return a dataframe. This dataframe is shown in app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-import streamlit
+import streamlit as st
+import preprocessor
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file=st.sidebar.file_uploader("Upload a file")
+if uploaded_file is not None:
+    bytes_data=uploaded_file.getvalue()
+    data=bytes_data.decode("utf-8")
+    df=preprocessor.preprocess(data)
+
+    
+    st.dataframe(df)
