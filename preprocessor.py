05517606e6793ee8636b48cf0c11b7e3144146b3
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-19 17:17:02 +0530

    modified preprocessor.py by creating a function preprocess which takes data of whatsapp chat from user using app.py and in this function, I have added all the codes from whatsapp-chat-analysis.ipynb which will return a dataframe df and this df is shown to user in frontend

--- preprocessor.py
+++ preprocessor.py
@@ -1,5 +1,8 @@
+import re
+import pandas as pd
+
 def preprocess(data):
-    pattern=r'\d{2}/\d{2}/\d{2},\s\d{1,2}:\d{2}\s[ap]m'
+    pattern='\d{2}/\d{2}/\d{2},\s\d{1,2}:\d{2}\s[ap]m'
 
     messages=re.split(pattern,data)[1:]
 
@@ -7,3 +10,31 @@ def preprocess(data):
 
     cleaned_dates=[entry.replace('\u202f','')for entry in dates]
 
+    df=pd.DataFrame({'user_message':messages,'message_date':cleaned_dates})
+
+    df['message_date'] = pd.to_datetime(df['message_date'],format='%d/%m/%y, %I:%M%p')
+    
+    df.rename(columns={'message-date':'date'},inplace=True)
+
+    users=[]
+    messages=[]
+    for message in df['user_message']:
+        entry=re.split('([\w\W]+?):\s', message)
+        if entry[1:]:
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user']=users
+    df['messages']=messages
+    df.drop(columns=['user_message'],inplace=True)
+
+    df['year']=df['message_date'].dt.year
+    df['month']=df['message_date'].dt.month_name()
+    df['day_of_month']=df['message_date'].dt.day
+    df['hour']=df['message_date'].dt.hour
+    df['minute']=df['message_date'].dt.minute
+
+    return df
