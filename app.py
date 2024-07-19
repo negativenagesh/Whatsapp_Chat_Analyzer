ba012a88171f8646f3aa644398191f639e6bbc85
Subrahmanya Gaonkar <gaonkarsub@gmail.com>
2024-07-19 17:42:18 +0530

    last modification

--- app.py
+++ app.py
@@ -9,5 +9,16 @@ if uploaded_file is not None:
     data=bytes_data.decode("utf-8")
     df=preprocessor.preprocess(data)
 
-    
+
     st.dataframe(df)
+
+    user_list=df['user'].unique().tolist()
+
+    user_list.remove("group_notification")
+    user_list.sort()
+    user_list.insert(0,"Overall")
+
+    st.sidebar.selectbox("Show analysis with respect to",user_list)
+
+    if st.sidebar.button("Show analysis"):
+        pass
